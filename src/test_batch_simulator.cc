60a00736926cf84be7e087700f12d988d976a9f9
Unknown <pangtao@mit.edu>
2022-01-19 17:50:56 -0800

    Unit test on parallel vs serial execution.

--- src/test_batch_simulator.cc
+++ src/test_batch_simulator.cc
@@ -25,71 +25,82 @@ MatrixXd CreateRandomMatrix(int n_rows, int n_cols, std::mt19937 &gen) {
   return MatrixXd::NullaryExpr(n_rows, n_cols, [&]() { return dis(gen); });
 }
 
-int main() {
-  QuasistaticSimParameters sim_params;
-  sim_params.gravity = Vector3d(0, 0, -10);
-  sim_params.nd_per_contact = 2;
-  sim_params.contact_detection_tolerance = 1.0;
-  sim_params.is_quasi_dynamic = true;
-  sim_params.gradient_from_active_constraints = true;
 
-  VectorXd Kp;
-  Kp.resize(2);
-  Kp << 50, 25;
-  const string robot_l_name = "arm_left";
-  const string robot_r_name = "arm_right";
+class TestBatchQuasistaticSimulator : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    QuasistaticSimParameters sim_params;
+    sim_params.gravity = Vector3d(0, 0, -10);
+    sim_params.nd_per_contact = 2;
+    sim_params.contact_detection_tolerance = 1.0;
+    sim_params.is_quasi_dynamic = true;
+    sim_params.gradient_from_active_constraints = true;
 
-  std::unordered_map<string, VectorXd> robot_stiffness_dict = {
-      {robot_l_name, Kp}, {robot_r_name, Kp}};
+    VectorXd Kp;
+    Kp.resize(2);
+    Kp << 50, 25;
+    const string robot_l_name = "arm_left";
+    const string robot_r_name = "arm_right";
 
-  const string object_name("sphere");
-  std::unordered_map<string, string> object_sdf_dict;
-  object_sdf_dict[object_name] = kObjectSdfPath;
+    std::unordered_map<string, VectorXd> robot_stiffness_dict = {
+        {robot_l_name, Kp}, {robot_r_name, Kp}};
 
-  auto q_sim_batch = BatchQuasistaticSimulator(
-      kModelDirectivePath, robot_stiffness_dict, object_sdf_dict, sim_params);
-  auto &q_sim = q_sim_batch.get_q_sim();
-  const auto name_to_idx_map = q_sim.GetModelInstanceNameToIndexMap();
-  const auto idx_l = name_to_idx_map.at(robot_l_name);
-  const auto idx_r = name_to_idx_map.at(robot_r_name);
-  const auto idx_o = name_to_idx_map.at(object_name);
+    const string object_name("sphere");
+    std::unordered_map<string, string> object_sdf_dict;
+    object_sdf_dict[object_name] = kObjectSdfPath;
 
-  ModelInstanceIndexToVecMap q0_dict = {{idx_o, Vector3d(0, 0.316, 0)},
-                                        {idx_l, Vector2d(-0.775, -0.785)},
-                                        {idx_r, Vector2d(0.775, 0.785)}};
+    q_sim_batch_ = std::make_unique<BatchQuasistaticSimulator>(
+        kModelDirectivePath, robot_stiffness_dict, object_sdf_dict, sim_params);
 
-  VectorXd q0 = q_sim.GetQFromQdict(q0_dict);
-  VectorXd u0 = q_sim.GetQaCmdFromQaCmdDict(q0_dict);
+    auto &q_sim = q_sim_batch_->get_q_sim();
+    const auto name_to_idx_map = q_sim.GetModelInstanceNameToIndexMap();
+    const auto idx_l = name_to_idx_map.at(robot_l_name);
+    const auto idx_r = name_to_idx_map.at(robot_r_name);
+    const auto idx_o = name_to_idx_map.at(object_name);
 
-  auto n_tasks = 11;
-  const int n_q = q_sim.get_plant().num_positions();
-  const double h = 0.1;
+    ModelInstanceIndexToVecMap q0_dict = {{idx_o, Vector3d(0, 0.316, 0)},
+                                          {idx_l, Vector2d(-0.775, -0.785)},
+                                          {idx_r, Vector2d(0.775, 0.785)}};
 
-  std::mt19937 gen(1);
-  MatrixXd u_batch =
-      0.1 * CreateRandomMatrix(n_tasks, q_sim.num_actuated_dofs(), gen);
-  u_batch.rowwise() += u0.transpose();
+    VectorXd q0 = q_sim.GetQFromQdict(q0_dict);
+    VectorXd u0 = q_sim.GetQaCmdFromQaCmdDict(q0_dict);
 
-  MatrixXd x_batch(n_tasks, n_q);
-  x_batch.setZero();
-  x_batch.rowwise() += q0.transpose();
-  cout << "Start" << endl;
+    const int n_q = q_sim.get_plant().num_positions();
+
+    std::mt19937 gen(1);
+    u_batch_ = 0.1 * CreateRandomMatrix(n_tasks_, q_sim.num_actuated_dofs(),
+                                        gen);
+    u_batch_.rowwise() += u0.transpose();
+
+    x_batch_.resize(n_tasks_, n_q);
+    x_batch_.setZero();
+    x_batch_.rowwise() += q0.transpose();
+  }
+
+  int n_tasks_{121};
+  double h_{0.1};
+  MatrixXd u_batch_, x_batch_;
+  std::unique_ptr<BatchQuasistaticSimulator> q_sim_batch_;
+};
+
+TEST_F(TestBatchQuasistaticSimulator, TestForwardDynamics) {
   MatrixXd x_next_batch_parallel =
-      q_sim_batch.CalcDynamicsParallel(x_batch, u_batch, h);
-  cout << "Parallel done" << endl;
+      q_sim_batch_->CalcDynamicsParallel(x_batch_, u_batch_, h_);
 
   MatrixXd x_next_batch_serial =
-      q_sim_batch.CalcDynamicsSingleThread(x_batch, u_batch, h);
-  cout << "Serial done" << endl;
+      q_sim_batch_->CalcDynamicsSingleThread(x_batch_, u_batch_, h_);
 
-  cout << "Diff "
-       << (x_next_batch_serial - x_next_batch_parallel)
-                  .matrix()
-                  .rowwise()
-                  .norm()
-                  .sum() /
-              n_tasks
-       << endl;
+  const double avg_diff = (x_next_batch_serial - x_next_batch_parallel)
+      .matrix()
+      .rowwise()
+      .norm()
+      .sum() /
+      n_tasks_;
+  EXPECT_LT(avg_diff, 1e-6);
+}
 
-  return 0;
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
