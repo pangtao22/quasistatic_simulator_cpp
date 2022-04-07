f35ae8929cb8a144f5414e82d077f5fe1d12ae99
Pang <pangtao@mit.edu>
2022-04-06 20:15:07 -0400

    Fix bugs in log_barrier_solver. It works!

--- src/log_barrier_solver.cc
+++ src/log_barrier_solver.cc
@@ -21,7 +21,8 @@ void QpLogBarrierSolver::SolvePhaseOne(
   // v_s is the concatenation of [v, s], where v is the vector of generalized
   // velocities of the system and s is the scalar slack variables.
   auto v_s = prog.NewContinuousVariables(n_v + 1, "v");
-  auto s = v_s[n_v];
+  const auto& v = v_s.head(n_v);
+  const auto& s = v_s[n_v];
 
   // G * v - e <= s  <==> [G, -1] * v_s <= e.
   MatrixXd G_1(n_f, n_v + 1);
@@ -32,6 +33,7 @@ void QpLogBarrierSolver::SolvePhaseOne(
   prog.AddLinearConstraint(
       G_1, VectorXd::Constant(n_f, -std::numeric_limits<double>::infinity()), e,
       v_s);
+  prog.AddBoundingBoxConstraint(-1, 1, v);
 
   solver_->Solve(prog, {}, {}, &mp_result_);
   if (!mp_result_.is_success()) {
@@ -39,14 +41,14 @@ void QpLogBarrierSolver::SolvePhaseOne(
   }
 
   const auto s_value = mp_result_.GetSolution(s);
-  if (s_value < -1e-6) {
+  if (s_value > -1e-6) {
     v0_ptr = nullptr;
     std::stringstream ss;
     ss << "Phase 1 cannot find a feasible solution. s = " << s_value << endl;
     throw std::runtime_error(ss.str());
   }
 
-  *v0_ptr = mp_result_.GetSolution();
+  *v0_ptr = mp_result_.GetSolution(v);
 }
 
 double QpLogBarrierSolver::CalcF(const Eigen::Ref<const Eigen::MatrixXd> &Q,
@@ -78,7 +80,7 @@ void QpLogBarrierSolver::CalcGradientAndHessian(
     drake::EigenPtr<Eigen::VectorXd> Df_ptr,
     drake::EigenPtr<Eigen::MatrixXd> H_ptr) {
   *H_ptr = Q * kappa;
-  *Df_ptr = b * kappa;
+  *Df_ptr = (Q * v + b) * kappa;
   for (int i = 0; i < G.rows(); i++) {
     double d = G.row(i) * v - e[i];
     *H_ptr += G.row(i).transpose() * G.row(i) / d / d;
@@ -102,11 +104,16 @@ QpLogBarrierSolver::Solve(const Eigen::Ref<const Eigen::MatrixXd> &Q,
   while (n_iters < iteration_limit_) {
     CalcGradientAndHessian(Q, b, G, e, v, kappa, &Df, &H);
     VectorXd dv = -H.llt().solve(Df);
-    double lambda_squared = -Df.transpose() * v;
-    if (lambda_squared < 1e-6) {
+    double lambda_squared = -Df.transpose() * dv;
+    if (lambda_squared / 2 < 1e-6) {
       converged = true;
       break;
     }
+//    cout << "------------------------------------------" << endl;
+//    cout << "Iter " << n_iters << endl;
+//    cout << "H\n" << H << endl;
+//    cout << "dv: " << dv.transpose() << endl;
+//    cout << "v: " << v.transpose() << endl;
 
     // back-tracking line search.
     double t = 1;
@@ -119,6 +126,7 @@ QpLogBarrierSolver::Solve(const Eigen::Ref<const Eigen::MatrixXd> &Q,
       }
       t *= beta_;
     }
+//    cout << "t " << t << endl;
     v += t * dv;
     n_iters++;
   }
---
+++ src/run_log_barrier_solver.cc
@@ -0,0 +1,28 @@
+#include "log_barrier_solver.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout;
+using std::endl;
+
+int main() {
+  const int n_v = 3;
+  MatrixXd Q(n_v, n_v);
+  Q.setIdentity();
+
+  VectorXd tau_h(n_v);
+  tau_h << 0.31390925,  0.1954178 , 0;
+
+  MatrixXd J(2, n_v);
+  J.row(0) << -1.,  1.,  1;
+  J.row(1) << 1.,  1., -1;
+
+  const double kappa{100}, h{0.1};
+
+  VectorXd phi_constraints(2);
+  phi_constraints.setZero();
+
+  auto solver = QpLogBarrierSolver();
+  solver.Solve(Q, -tau_h, -J, phi_constraints / h, kappa);
+  return 0;
+}
